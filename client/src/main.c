deaf8ee15527fdc2521b7219e4595f6f7e19d569
Stig Bjørlykke <stig.bjorlykke@nordicsemi.no>
2019-01-17 15:12:31 +0100

    Set ACL permissions for server id 1000
    Use the same ACL permission as for server id 102 for now.

--- client/src/main.c
+++ client/src/main.c
@@ -2102,8 +2102,15 @@ static void app_lwm2m_create_objects(void)
 
     (void)lwm2m_acl_permissions_add((lwm2m_instance_t *)&m_instance_device,
                                     (LWM2M_PERMISSION_READ | LWM2M_PERMISSION_WRITE |
-                                     LWM2M_PERMISSION_DELETE | LWM2M_PERMISSION_EXECUTE),
+                                     LWM2M_PERMISSION_DELETE | LWM2M_PERMISSION_EXECUTE |
+                                     LWM2M_PERMISSION_OBSERVE),
                                     102);
+
+    (void)lwm2m_acl_permissions_add((lwm2m_instance_t *)&m_instance_device,
+                                    (LWM2M_PERMISSION_READ | LWM2M_PERMISSION_WRITE |
+                                     LWM2M_PERMISSION_DELETE | LWM2M_PERMISSION_EXECUTE |
+                                     LWM2M_PERMISSION_OBSERVE),
+                                    1000);
 
     (void)lwm2m_coap_handler_instance_add((lwm2m_instance_t *)&m_instance_device);
 
@@ -2155,6 +2162,12 @@ static void app_lwm2m_create_objects(void)
                                      LWM2M_PERMISSION_DELETE | LWM2M_PERMISSION_EXECUTE |
                                      LWM2M_PERMISSION_OBSERVE),
                                     102);
+
+    (void)lwm2m_acl_permissions_add((lwm2m_instance_t *)&m_instance_conn_mon,
+                                    (LWM2M_PERMISSION_READ | LWM2M_PERMISSION_WRITE |
+                                     LWM2M_PERMISSION_DELETE | LWM2M_PERMISSION_EXECUTE |
+                                     LWM2M_PERMISSION_OBSERVE),
+                                    1000);
 
     (void)lwm2m_coap_handler_instance_add((lwm2m_instance_t *)&m_instance_conn_mon);
 }
