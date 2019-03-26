38ab81108e88fb78154571fb078b34f4bbdfb415
Stig Bjørlykke <stig.bjorlykke@nordicsemi.no>
2019-03-26 14:26:44 +0100

    device: Use IMEI as serial number

--- client/src/lwm2m_device.c
+++ client/src/lwm2m_device.c
@@ -15,6 +15,7 @@
 #include <lwm2m_objects_plain_text.h>
 #include <common.h>
 #include <at_interface.h>
+#include <main.h>
 
 #define VERIZON_RESOURCE 30000
 
@@ -239,7 +240,7 @@ void lwm2m_device_init(void)
     m_instance_device.manufacturer.len = strlen(m_instance_device.manufacturer.p_val);
     m_instance_device.model_number.p_val = "nRF91";
     m_instance_device.model_number.len = strlen(m_instance_device.model_number.p_val);
-    m_instance_device.serial_number.p_val = "1234567890";
+    m_instance_device.serial_number.p_val = app_imei_get();
     m_instance_device.serial_number.len = strlen(m_instance_device.serial_number.p_val);
     m_instance_device.firmware_version.p_val = "1.0";
     m_instance_device.firmware_version.len = strlen(m_instance_device.firmware_version.p_val);
