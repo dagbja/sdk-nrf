a2a5faee5d451efe4e3d43e4df390d954b38db00
Robert Lubos <robert.lubos@nordicsemi.no>
2019-05-17 14:58:23 +0200

    vzw_lwm2m: Update library build script
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- project/export.sh
+++ project/export.sh
@@ -2,8 +2,8 @@
 
 project_dir="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-rm -rf $project_dir/../../nrfxlib/lwm2m/lib
-rm -rf $project_dir/../../nrfxlib/lwm2m/include
+rm -rf $project_dir/../../nrfxlib/lwm2m/nrf_lwm2m
+rm -rf $project_dir/../../nrfxlib/lwm2m/nrf_vzw_lwm2m
 
-cp -r $project_dir/output/lib $project_dir/../../nrfxlib/lwm2m
-cp -r $project_dir/output/include $project_dir/../../nrfxlib/lwm2m
+cp -r $project_dir/output/nrf_lwm2m $project_dir/../../nrfxlib/lwm2m
+cp -r $project_dir/output/nrf_vzw_lwm2m $project_dir/../../nrfxlib/lwm2m
--- project/prepare.sh
+++ project/prepare.sh
@@ -1,17 +1,42 @@
 #!/usr/bin/env bash
 
 project_dir="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-lib_name="libnrf_lwm2m.a"
 output_dir="output"
 cpu_variant="cortex-m33"
-declare -a lib_variant=("hard-float"
-			"soft-float"
-			"softfp-float")
+
+declare -a lib_variants=("hard-float"
+			 "soft-float"
+			 "softfp-float")
+
+declare -a lib_names=("nrf_lwm2m"
+		      "nrf_vzw_lwm2m")
+declare -A lib_paths=([${lib_names[0]}]="lwm2m"
+		      [${lib_names[1]}]="vzw_lwm2m")
+
+declare -a api_headers_nrf_lwm2m=("lwm2m_cfg.h"
+				  "lwm2m_api.h"
+				  "lwm2m_objects.h"
+				  "lwm2m_remote.h"
+				  "lwm2m_acl.h"
+				  "lwm2m_objects_tlv.h"
+				  "lwm2m_tlv.h"
+				  "lwm2m_objects_plain_text.h"
+				  "lwm2m.h")
+
+declare -a api_headers_nrf_vzw_lwm2m=("common.h"
+				      "lwm2m_conn_mon.h"
+				      "lwm2m_device.h"
+				      "lwm2m_firmware.h"
+				      "lwm2m_instance_storage.h"
+				      "lwm2m_retry_delay.h"
+				      "lwm2m_security.h"
+				      "lwm2m_server.h")
 
 rm -rf $project_dir/$output_dir
 
 # Build and copy libraries
-for i in "${lib_variant[@]}"
+
+for i in "${lib_variants[@]}"
 do
 	echo "Preparing $i"
 	rm -rf $project_dir/build-$i
@@ -19,33 +44,36 @@ do
 	cd $project_dir/build-$i
 
 	cmake -GNinja -DBOARD=nrf9160_pca10090ns -DOVERLAY_CONFIG=overlay-$i.conf ..
-	ninja $lib_name
 
-	mkdir -p $project_dir/$output_dir/lib/$cpu_variant/$i
-	cp $project_dir/build-$i/zephyr/lwm2m/lib/lwm2m/$lib_name $project_dir/$output_dir/lib/$cpu_variant/$i/$lib_name
+	for j in "${lib_names[@]}"
+	do
+		ninja lib$j.a
+
+		mkdir -p $project_dir/$output_dir/$j/lib/$cpu_variant/$i
+		cp $project_dir/build-$i/zephyr/lwm2m/lib/${lib_paths[$j]}/lib$j.a $project_dir/$output_dir/$j/lib/$cpu_variant/$i
+	done
 done
 
 # Copy headers
-declare -a api_headers=("lwm2m_cfg.h"
-			"lwm2m_api.h"
-			"lwm2m_objects.h"
-			"lwm2m_remote.h"
-			"lwm2m_acl.h"
-			"lwm2m_objects_tlv.h"
-			"lwm2m_tlv.h"
-			"lwm2m_objects_plain_text.h"
-			"lwm2m.h")
 
-mkdir mkdir -p $project_dir/$output_dir/include
+mkdir -p $project_dir/$output_dir/nrf_lwm2m/include
 
-for i in "${api_headers[@]}"
+for i in "${api_headers_nrf_lwm2m[@]}"
 do
-	cp $project_dir/../lib/lwm2m/include/$i $project_dir/$output_dir/include
+	cp $project_dir/../lib/lwm2m/include/$i $project_dir/$output_dir/nrf_lwm2m/include
+done
+
+mkdir -p $project_dir/$output_dir/nrf_vzw_lwm2m/include
+
+for i in "${api_headers_nrf_vzw_lwm2m[@]}"
+do
+	cp $project_dir/../lib/vzw_lwm2m/include/$i $project_dir/$output_dir/nrf_vzw_lwm2m/include
 done
 
 # Replace Kconfig macros with acual values
-autoconf_file="$project_dir/build-${lib_variant[0]}/zephyr/include/generated/autoconf.h"
-target_file="$project_dir/$output_dir/include/lwm2m_cfg.h"
+
+autoconf_file="$project_dir/build-${lib_variants[0]}/zephyr/include/generated/autoconf.h"
+target_file="$project_dir/$output_dir/${lib_names[0]}/include/lwm2m_cfg.h"
 
 while read -r def conf val
 do
