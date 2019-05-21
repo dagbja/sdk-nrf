6891c05f1f481e4b05cdafaffcb549022263588a
Stig Bjørlykke <stig.bjorlykke@nordicsemi.no>
2019-05-21 09:06:44 +0200

    Increase NVS_SECTOR_COUNT to 3
    Because of a bug in Zephyr NVS garbage collection we should use
    3 sectors for now.

--- client/src/lwm2m_instance_storage.c
+++ client/src/lwm2m_instance_storage.c
@@ -17,7 +17,7 @@
 
 /* NVS-related defines */
 #define NVS_SECTOR_SIZE    DT_FLASH_ERASE_BLOCK_SIZE    /* Multiple of FLASH_PAGE_SIZE */
-#define NVS_SECTOR_COUNT   2                            /* At least 2 sectors */
+#define NVS_SECTOR_COUNT   3                            /* At least 2 sectors */
 #define NVS_STORAGE_OFFSET DT_FLASH_AREA_STORAGE_OFFSET /* Start address of the filesystem in flash */
 
 static struct nvs_fs fs = {
