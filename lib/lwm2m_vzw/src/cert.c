461a47820c617a8d3c5750a3f3388c2a4e8ab6ee
Emanuele Di Santo <emdi@nordicsemi.no>
2019-10-07 10:53:36 +0200

    lwm2m_vzw: do not provision certificates at each boot
    Only provision certificates if they are not found.
    
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>

--- lib/lwm2m_vzw/src/cert.c
+++ lib/lwm2m_vzw/src/cert.c
@@ -19,30 +19,23 @@ BUILD_ASSERT_MSG(sizeof(ca_chain) < 4096, "CA is too large");
 int cert_provision(void)
 {
 	int err;
+	bool provisioned;
+
+	uint8_t dummy;
+	ARG_UNUSED(dummy);
 
 	nrf_sec_tag_t tag = CONFIG_NRF_LWM2M_VZW_SEC_TAG;
 
 	if (CONFIG_NRF_LWM2M_VZW_SEC_TAG == -1) {
-		LWM2M_INF("No certificates to be provisioned.");
+		LWM2M_WRN("No certificates to be provisioned.");
 		return 0;
 	}
 
-	/* Here we simply delete and provision the certificates at each boot.
-	 * This clearly has implications on the device lifetime.
-	 *
-	 * TODO:
-	 * - fix nrf_inbuilt_key_exists() implementation
-	 *   to not return an error when the key does not exist,
-	 *   since it has an output parameter
-	 * - update the documentation accordingly
-	 * - use it here to avoid deleting and
-	 *   re-provisioning certificates at each boot
-	 */
-
-	/* Delete certificate */
-	err = nrf_inbuilt_key_delete(tag, NRF_KEY_MGMT_CRED_TYPE_CA_CHAIN);
-	if (err) {
-		/* Ignore errors, which can happen if the key doesn't exist. */
+	err = nrf_inbuilt_key_exists(tag, NRF_KEY_MGMT_CRED_TYPE_CA_CHAIN,
+				     &provisioned, &dummy);
+	if (!err && provisioned) {
+		LWM2M_INF("Certificates found, tag %lu", tag);
+		return 0;
 	}
 
 	err = nrf_inbuilt_key_write(tag, NRF_KEY_MGMT_CRED_TYPE_CA_CHAIN,
