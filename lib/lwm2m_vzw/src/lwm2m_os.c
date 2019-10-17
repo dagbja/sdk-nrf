dac69c14163c37fe8bb4b483e775a3f92560f505
Christopher Métrailler <christopher.metrailler@nordicsemi.no>
2019-10-17 13:22:57 +0200

    lib: lwm2m_carrier: os: Change direct call to secure service to implicit
    Backport commit a1af2e5813fb7009892db87a8ed5ad5bc9223b61.
    
    Signed-off-by: Christopher Métrailler <christopher.metrailler@nordicsemi.no>

--- lib/lwm2m_vzw/src/lwm2m_os.c
+++ lib/lwm2m_vzw/src/lwm2m_os.c
@@ -12,6 +12,7 @@
 #include <logging/log.h>
 #include <secure_services.h>
 #include <misc/util.h>
+#include <misc/reboot.h>
 
 /* NVS-related defines */
 
@@ -62,7 +63,7 @@ int lwm2m_os_sleep(int ms)
 
 void lwm2m_os_sys_reset(void)
 {
-	spm_request_system_reboot();
+	sys_reboot(SYS_REBOOT_COLD);
 	CODE_UNREACHABLE;
 }
 
