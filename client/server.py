a5bdc82aef30521810e59c051c415095776681d7
Kacper Radoszewski <kacper.radoszewski@nordicsemi.no>
2020-02-25 10:29:08 +0100

    Update the script to push client bootstrap configuration to Leshan
    Glenn and I have previously modified the script to make it easier to
    configure, without having to translate the PSK and public key to decimal
    ourselves. I have also updated the PSK in accordance with the one that
    appears in the library.

--- client/server.py
+++ client/server.py
@@ -1,9 +1,8 @@
 # Based on https://github.com/eclipse/leshan/issues/374.
 # Author: chme@nordicsemi.no
 #
-# Key is "glennssecret".
-# Public key is "urn:imei-msisdn:352656100003501-0011600025".
-# Has to be converted to decimal.
+# Key is "nordicsecret".
+# Public key is "urn:imei:xxxxxxxxxxxxxxx".
 
 import requests
 import json
@@ -11,54 +10,52 @@ import json
 # URL of the server
 BASE_URL = "http://vmi36865.contabo.host:8081/"
 
-# The client endpoint here
-#url = BASE_URL + "api/bootstrap/urn:imei-msisdn:352656100003501-0011600025"
-# "publicKeyOrId": [117, 114, 110, 58, 105, 109, 101, 105, 45, 109, 115, 105, 115, 100, 110, 58, 51, 53, 50, 54, 53, 54, 49, 48, 48, 48, 48, 51, 53, 48, 49, 45, 48, 48, 49, 49, 54, 48, 48, 48, 50, 53],
-url = BASE_URL + "api/bootstrap/urn:imei-msisdn:352656100001844-0011600025"
-
+urn = "urn:imei:352656100003501"
+password = "nordicsecret"
+url = BASE_URL + "api/bootstrap/" + urn
 
 data = {"servers": {
     "1": {"shortId": 102},
-    "2": {"shortId": 1000}},
+    "3": {"shortId": 1000}},
     "security": {  # bootstrap
     "0": {"uri": "coaps://vmi36865.contabo.host:5784",
           "bootstrapServer": True,
           "securityMode": "PSK",
-          "publicKeyOrId": [117, 114, 110, 58, 105, 109, 101, 105, 45, 109, 115, 105, 115, 100, 110, 58, 51, 53, 50, 54, 53, 54, 49, 48, 48, 48, 48, 49, 56, 52, 52, 45, 48, 48, 49, 49, 54, 48, 48, 48, 50, 53],
+          "publicKeyOrId": [ord(i) for i in urn],
           "serverPublicKey": [],
-          "secretKey": [103, 108, 101, 110, 110, 115, 115, 101, 99, 114, 101, 116],
+          "secretKey": [ord(i) for i in password],
           "smsSecurityMode": "NO_SEC",
           "smsBindingKeyParam": [],
           "smsBindingKeySecret": [],
           "serverSmsNumber": "+3343577911",
           "serverId": "0",
-          "clientOldOffTime": 20},
+          "clientHoldOffTime": 20},
     # DM server
     "1": {"uri": "coaps://vmi36865.contabo.host:5684",
           "bootstrapServer": False,
           "securityMode": "PSK",
-          "publicKeyOrId": [117, 114, 110, 58, 105, 109, 101, 105, 45, 109, 115, 105, 115, 100, 110, 58, 51, 53, 50, 54, 53, 54, 49, 48, 48, 48, 48, 49, 56, 52, 52, 45, 48, 48, 49, 49, 54, 48, 48, 48, 50, 53],
+          "publicKeyOrId": [ord(i) for i in urn],
           "serverPublicKey": [],
-          "secretKey": [103, 108, 101, 110, 110, 115, 115, 101, 99, 114, 101, 116],
+          "secretKey": [ord(i) for i in password],
           "smsSecurityMode": "NO_SEC",
           "smsBindingKeyParam": [],
           "smsBindingKeySecret": [],
           "serverSmsNumber": "+3343577464",
           "serverId": "102",
-          "clientOldOffTime": 1},
+          "clientHoldOffTime": 1},
     # data repository server
-    "2": {"uri": "coaps://vmi36865.contabo.host:6684",
+    "3": {"uri": "coaps://vmi36865.contabo.host:6684",
           "bootstrapServer": False,
           "securityMode": "PSK",
-          "publicKeyOrId": [117, 114, 110, 58, 105, 109, 101, 105, 45, 109, 115, 105, 115, 100, 110, 58, 51, 53, 50, 54, 53, 54, 49, 48, 48, 48, 48, 49, 56, 52, 52, 45, 48, 48, 49, 49, 54, 48, 48, 48, 50, 53],
+          "publicKeyOrId": [ord(i) for i in urn],
           "serverPublicKey": [],
-          "secretKey": [103, 108, 101, 110, 110, 115, 115, 101, 99, 114, 101, 116],
+          "secretKey": [ord(i) for i in password],
           "smsSecurityMode": "NO_SEC",
           "smsBindingKeyParam": [],
           "smsBindingKeySecret": [],
           "serverSmsNumber": "+3343577464",
           "serverId": "1000",
-          "clientOldOffTime": 1}
+          "clientHoldOffTime": 1}
 }}
 
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
